cea2990fc558b4a3f45d0e0ff5e514a6cefacb04
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-19 17:38:23 +0100

    feat: add axios call to pizza api
    - add getPizzas method

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <div id="app">
-    <h1>Pizze</h1>
+    <div class="container">
+      <h1>Pizze</h1>
+      <ul>
+        <li v-for="pizza in pizzas" :key="pizza.id">
+          {{ pizza.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
 
+  },
+
+  data() {
+    return {
+      apiUrl: 'http://localhost:8080/api/1/',
+      pizzas: [],
+    }
+  },
+
+  methods: {
+    getPizzas() {
+      axios.get(this.apiUrl + "pizza/all")
+        .then(response => {
+          this.pizzas = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+
+  mounted() {
+    this.getPizzas()
   }
 }
 </script>
